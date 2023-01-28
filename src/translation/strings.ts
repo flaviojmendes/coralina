a35952acae0c2e983aa07cf74e2d8a8b1ee220b5
Flavio Mendes <flaviojmendes@gmail.com>
2023-01-28 00:00:37 +0000

    adding support to idioms

---
+++ src/translation/strings.ts
@@ -0,0 +1,67 @@
+export const ptBr = {
+  language: "pt",
+  onceUponATime: "Era uma vez",
+  withTheirFriends: "juntamente com",
+  against: "contra",
+  in: "em",
+  theme: "com tema",
+
+  mainCharacter: "protagonista",
+  secondaryCharacters: "coadjuvante(s)",
+  villain: "antagonista",
+  detailPlace: "detalhe, local, etc",
+  select: "selecione",
+
+  romance: "Romance",
+  child: "Infantil",
+  scyFy: "Ficção Científica",
+  horror: "Terror",
+  motivational: "Motivacional",
+
+  myStory: "Minha História",
+  myStories: "Minhas Histórias",
+
+  writingStory: "escrevendo sua história..(isso pode demorar alguns minutos)",
+  storyError:
+    "Desculpe, algo errado ocorreu. Favor tentar novamente com personagens diferentes.",
+
+  seeFullStory: "Ver história completa",
+  seeAllStories: "Ver todas as histórias",
+
+  writeMyStory: "Escrever minha história",
+  createdBy: "Feito com 💜 por"
+};
+
+export const en = {
+  language: "en",
+  onceUponATime: "Once upon a time",
+  withTheirFriends: "with their friends",
+  against: "against",
+  in: "in",
+  theme: "with the theme",
+
+  mainCharacter: "main character",
+  secondaryCharacters: "secondary(s)",
+  villain: "villain",
+  detailPlace: "detail, place, etc",
+  select: "select",
+
+  romance: "Romance",
+  child: "Child",
+  scyFy: "Scy-Fy",
+  horror: "Horror",
+  motivational: "Motivational",
+
+  myStory: "My Story",
+  myStories: "My Stories",
+
+  writingStory: "writing your story.. (this may take a few minutes)",
+  storyError:
+    "Sorry, something wrong happened. Please try again with different inputs.",
+
+  seeFullStory: "See full story",
+  seeAllStories: "See all stories",
+
+  writeMyStory: "Write my story",
+  createdBy: "Made with 💜 by"
+};
